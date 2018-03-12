228b65ad023d4ec6f3b6480bea426c324863888c
이제형 <ljh710m@hanmail.net>
2018-03-12 17:01:55 +0900

    삽입 데이터

---
+++ CBangProj/WebContent/SQL/data.sql
@@ -0,0 +1,7 @@
+/* ADMIN_PRIVILEGE(관리자 권한)*/
+insert into ADMIN_PRIVILEGE values(ADMIN_PRIVILEGE_CODE_SEQ.NEXTVAL,'최고 관리자');
+insert into ADMIN_PRIVILEGE values(ADMIN_PRIVILEGE_CODE_SEQ.NEXTVAL,'승인 대기');
+
+/*CBANG_ADMIN(관리자)*/
+insert into CBANG_ADMIN values(ADMIN_CODE_SEQ.NEXTVAL,'admin','1234','관리자','admin@cbang.com',1);
+
