70f36be5c8fc14e8dd948d89baf48e87a9afeeb1
YoungJoon <shina48@naver.com>
2018-03-27 19:58:37 +0900

    엿같은 페이징때문에 바꾼 쿼리랑 메소드

--- CBangProj/src/command/MembershipHandler.java
+++ CBangProj/src/command/MembershipHandler.java
@@ -1,13 +1,18 @@
 package command;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import model.AdminPrivilegeDto;
 import model.MembershipDto;
 import mvc.command.CommandHandler;
 import service.MembershipService;
+import util.MembershipPagingUtil;
+import util.SimplePagingUtil;
 
 public class MembershipHandler implements CommandHandler{
 	
@@ -15,11 +20,49 @@ public class MembershipHandler implements CommandHandler{
 	
 	@Override  
 	public String process(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+		String mode = req.getParameter("mode");
 		
-		List<MembershipDto> list = service.selectList(1, 10);
+		if(mode != null) {
+			/*switch(mode) {
+			case "membershipList":
+				req.setAttribute("", "");
+			}*/
+		}
+		
+		sendList(req);
+		
+		return "/backend/membership.jsp";
+	}
+	
+	private void sendList(HttpServletRequest req) {
+		//페이징을 위한 로직 시작]		
+		//전체 레코드 수
+		int totalRecordCount = service.getTotalRowCount();
+		int pageSize = 5;
+		int blockPage = 5;
+		int nowPage = 1;
+		/*if(req.getParameter("mode") != null)*/ nowPage = Integer.parseInt(req.getParameter("nowPage"));
+		
+		//전체 페이지수]
+		int totalPage =(int)Math.ceil((double)totalRecordCount/pageSize);		
+		//시작 및 끝 ROWNUM구하기]
+		if(nowPage > totalPage) nowPage = totalPage;
+		
+		int start= (nowPage-1)*pageSize +1;
+		int end  = nowPage*pageSize;		
+		//페이징을 위한 로직 끝]
+		
+		List<MembershipDto> list = service.selectList(start, end);
+		String pagingText = MembershipPagingUtil.pagingText(totalRecordCount, pageSize, blockPage, nowPage, null);
+		Map<String, Integer> map = new HashMap<String, Integer>();
+		map.put("totalRecordCount", totalRecordCount);
+		map.put("pageSize", pageSize);
+		map.put("blockPage", blockPage);
+		map.put("nowPage", nowPage);
+		
 		req.setAttribute("list", list);
-		
-		return "/backend/admin/membership.jsp";
+		req.setAttribute("paging", pagingText);
+		req.setAttribute("map", map);
 	}
 	
 }
--- CBangProj/src/service/MembershipService.java
+++ CBangProj/src/service/MembershipService.java
@@ -7,6 +7,7 @@ import java.util.List;
 import javax.management.RuntimeErrorException;
 import javax.servlet.ServletContext;
 
+import jdbc.JdbcUtil;
 import jdbc.connection.ConnectionProvider;
 import model.MembershipDto;
 import model.dao.MembershipDao;
@@ -23,6 +24,24 @@ public class MembershipService {
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
 		}
+	}
+
+	public int getTotalRowCount() {
+		Connection conn = null;
+		int totalRowCount=0;
+		try {
+			conn = ConnectionProvider.getConnection();
+			conn.setAutoCommit(false);
+			
+			totalRowCount = dao.getTotalRecordCount(conn);
+			
+		} catch(SQLException e) {
+			JdbcUtil.rollback(conn);			
+			throw new RuntimeException(e);
+		} finally {
+			JdbcUtil.close(conn);
+		}
+		return totalRowCount;
 	}
 	
 }
